299f8fe382121414b2670b6466454b97c01a5a61
hlays <lays.hidani@gmail.com>
2019-03-11 17:10:43 -0300

    add exerc sum primes

--- learning-js/lista-de-exerc-js/fcc-js/interm-algorithm-scripting.js
+++ learning-js/lista-de-exerc-js/fcc-js/interm-algorithm-scripting.js
@@ -312,3 +312,35 @@ function sumFibs(num) {
 
 // teste
 console.log('sumFibs(4): ', sumFibs(4));
+
+
+// -------------------------------------------------------------------
+
+console.log('-> Sum All Primes');
+
+// Sum all the prime numbers up to and including the provided number.
+
+// A prime number is defined as a number greater than one and having only two divisors, one and itself. For example, 2 is a prime number because it's only divisible by one and two.
+
+function sumPrimes(num) {
+  function isPrime(val) {
+    for(let i = 2; i < val; i++){
+      if (val % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  let sum = 0;
+
+  for(let j = 2; j <= num; j++){   
+    if(isPrime(j)){
+      sum += j;
+    }
+  }
+  return sum;
+}
+
+// teste
+console.log(sumPrimes(10));

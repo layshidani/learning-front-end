fc0236b3aae246e93f26bdd84c451ed2cc1a8a8d
hlays <lays.hidani@gmail.com>
2019-03-16 20:32:13 -0300

    Drop it

--- learning-js/lista-de-exerc-js/fcc-js/interm-algorithm-scripting.js
+++ learning-js/lista-de-exerc-js/fcc-js/interm-algorithm-scripting.js
@@ -344,3 +344,32 @@ function sumPrimes(num) {
 
 // teste
 console.log(sumPrimes(10));
+
+
+
+// -------------------------------------------------------------------
+
+console.log('-> Drop it');
+
+// Given the array arr, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated element is passed through it.
+
+// Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
+
+function dropElements(arr, func) {
+  // Drop them elements.
+  let newArr = arr.filter((n) => func(n));
+  return newArr;
+}
+
+// teste
+console.log(dropElements([1, 2, 3], function(n) {return n < 3; }));
+console.log(dropElements([0, 1, 0, 1], function(n) {return n === 1;}));
+console.log(dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}));
+console.log(dropElements([1, 2, 3, 4], function(n) {return n > 5;}));
+
+// -------------------------------------------------------------------
+
+console.log('-> Steamroller');
+
+// Flatten a nested array. You must account for varying levels of nesting.
+

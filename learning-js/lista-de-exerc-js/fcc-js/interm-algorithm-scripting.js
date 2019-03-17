b6d02991ada874b4abf5c443c4bd8b34e417ab21
hlays <lays.hidani@gmail.com>
2019-03-16 22:18:14 -0300

    addTogether (not solved yet)

--- learning-js/lista-de-exerc-js/fcc-js/interm-algorithm-scripting.js
+++ learning-js/lista-de-exerc-js/fcc-js/interm-algorithm-scripting.js
@@ -427,3 +427,31 @@ function truthCheck(collection, pre) {
 
 // teste
 console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
+
+
+// -------------------------------------------------------------------
+
+console.log('-> Arguments Optional');
+
+// Create a function that sums two arguments together. If only one argument is provided, then return a function that expects one argument and returns the sum.
+// For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
+// Calling this returned function with a single argument will then return the sum:
+// var sumTwoAnd = addTogether(2);
+// sumTwoAnd(3) returns 5.
+// If either argument isn't a valid number, return undefined.
+
+function addTogether() {
+  let args = Array.from(arguments);
+  
+  if (args.some(num => typeof num !== 'number')) {
+    return undefined;
+  } else if (args.length > 1) {
+    return args.reduce((sum, atual) => sum += atual, 0);
+  } else {
+    
+  }
+
+}
+
+// teste
+addTogether(2,3);
